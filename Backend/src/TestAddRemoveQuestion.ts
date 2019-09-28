3b60b47c6a4e04315711a8ca47a6bbd5a30ac1fa
Alex <awalte11@students.towson.edu>
2019-09-28 17:28:30 -0400

    adding unit test skeletons.
    Backend question skeletons first

---
+++ Backend/src/TestAddRemoveQuestion.ts
@@ -0,0 +1,33 @@
+//script goes here
+
+
+//Currently assuming 4-choice multiple question
+//Add Question
+function addQuestionTest1(){
+    //No question text
+    
+}
+
+function addQuestionTest2(){
+    //Incorrect answer number
+}
+
+function addQuestionTest3(){
+    //No correct answer given
+}
+
+function addQuestionTest4(){
+    //Everything works
+}
+
+
+function removeQuestionTest1(){
+    //Bad ID
+}
+
+function removeQuestionTest2(){
+    //It works
+}
+
+
+
---
+++ Backend/src/TestGetQuestions.ts
@@ -0,0 +1,70 @@
+//Setup and teardown goes here
+
+
+function TestGetQuestion1()
+{
+    //bad ID
+}
+
+function TestGetQuestion2()
+{
+    //bad format
+}
+
+function TestGetQuestion3()
+{
+    //works
+}
+
+function TestGetQuestionList1() {
+    //negative/zero questions requested
+}
+
+function TestGetQuestionList2() {
+    //more questions requested than in bank
+}
+
+function TestGetQuestionList3() {
+    //more questions requested than in bank for type (practical)
+}
+
+function TestGetQuestionList4() {
+    //more questions requested than in bank for type (theoretical)
+}
+
+
+
+function TestGetQuestionList5() {
+    //one question, practical
+}
+
+
+function TestGetQuestionList6() {
+    //two question, practical
+}
+function TestGetQuestionList7() {
+    //twenty question, practical
+}
+function TestGetQuestionList8() {
+    //one question, theory
+}
+
+
+function TestGetQuestionList9() {
+    //two question, theory
+}
+function TestGetQuestionList10() {
+    //twenty question, theory
+}
+function TestGetQuestionList11() {
+    //one question, mix
+}
+
+
+function TestGetQuestionList12() {
+    //two question, mix
+}
+function TestGetQuestionList13() {
+    //twenty question, mix
+}
+
