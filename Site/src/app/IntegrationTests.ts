84eabe2b4a4f9b53770e7feabaf7dec15b180158
Alex <awalte11@students.towson.edu>
2019-09-30 15:09:47 -0400

    Test plans, and integration test skeleton.

---
+++ Site/src/app/IntegrationTests.ts
@@ -0,0 +1,70 @@
+//setup goes here
+
+
+//teardown goes here
+
+
+function CreateAccountTest()
+{
+    //create account
+}
+
+function CreateAccountFailureTest()
+{
+    //test account creation fail states from this point
+}
+
+function ChangePasswordTest()
+{
+    //change PW
+
+}
+
+function LoginTestSuccess()
+{
+    //test login
+}
+
+function LoginTestFail()
+{
+    //test login fail states
+}
+
+function TakeQuizTest()
+{
+    //simulate taking quiz. 
+    //Unsure how to automate this given amount of user involvement?
+}
+
+function GetReportTest()
+{
+    //gets report
+}
+
+function GetReportNoAccessTest()
+{
+    //checks that people can't get reports they shouldn't
+    
+}
+
+
+function SocialMediaTest()
+{
+    //simulates brag-post
+}
+
+function ParentLinkTest()
+{
+    //simulate parent link activation
+}
+
+function ParentLinkRemoveTest()
+{
+    //simulate parent link removal
+}
+
+function ParentViewTest()
+{
+    //test parent link view
+}
+
