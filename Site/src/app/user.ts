c366f9e869c975f2ab6f984eebfdf6b7747d545b
awalte11 <awalte11@students.towson.edu>
2019-10-08 14:31:50 -0400

    more skeletons

--- Site/src/app/user.ts
+++ Site/src/app/user.ts
@@ -1,12 +1,12 @@
 import { QuizRecord } from 'src/app/quiz_record';
 
-export class user {
+export class User {
 	userName : String;
 	email : String;
 	firstName : String;
 	lastName : String;
 	records : QuizRecord[];
-	guardianOf : String[]; //User names this has guardian view access for
-	guardians : String[]; //User names that have guardian view access to this
+	parentOf : String[]; //User names this has parent view access for
+	parents : String[]; //User names that have parent view access to this
 	
 }
--- Site/src/app/users.service.ts
+++ Site/src/app/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { QuizRecord } from 'src/app/quiz_record';
- : 
+import { User } from 'src/app/user';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,12 +10,63 @@ export class UsersService {
   constructor() { }
   //Assumptions made - login/verification handled elsewhere. Username/password setup handled there
   
-  
+  /**
+  * gets the records of the user with the given account
+  * @param recordsOf name of target user
+  */
   async getRecords( recordsOf : string ) : Promise<QuizRecord[]>
   {
 	var records: QuizRecord[];
 
     return records;  
+	  
+  }
+  
+   /**
+  * set up parent/child view relationship
+  * @param parentName name of parent user 
+  * @param childBame of child user
+  */
+  async setParent ( parentName : string, childName : string) 
+  {
+	  
+	  
+  }
+  
+  /**
+  * remove parent/child view relationship
+  * @param parentName name of parent user 
+  * @param childBame of child user
+  */
+  async unsetParent ( parentName : string, childName : string) 
+  {
+	  
+	  
+  }
+  
+  async createUserRecord ( username : string, email : string, firstName : string, lastName : string ) : Promise<User>
+  {
+	  var user : User;
+	  user.userName = username;
+	  user.email = email;
+	  user.firstName = firstName;
+	  user.lastName = lastName;
+	  //TODO send to DB
+	  
+	  
+	  return user;
+  }
+  
+  async getUserRecord ( username : string ) : Promise<User>
+  {
+	  var user : User;
+	  
+	  return user;	  
+  }
+  
+  async addUserToRecord ( record : QuizRecord ) 
+  {
+	  
 	  
   }
   
